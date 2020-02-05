f291099083a0d467a4c657514f8639184842d7c4
hadiazb <hadiazb@unal.edu.co>
2020-02-05 18:17:52 -0500

    Cambios para la secuencia de colores

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 const btnEmpezar = document.getElementById('btnEmpezar')
+const ULTIMO_NIVEL = 10
 
 class Juego {
   constructor() {
     this.inicializar()
     this.generarSecuencia()
-    this.siguienteNivel()
+    setTimeout(this.siguienteNivel, 1000)
   }
 
   inicializar() {
+    this.siguienteNivel = this.siguienteNivel.bind(this)
+    this.elegirColor = this.elegirColor.bind(this)
     btnEmpezar.classList.add('hide')
     this.nivel = 7
     this.colores = {
@@ -22,11 +25,14 @@ class Juego {
     }
   }
   generarSecuencia(){
-    this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+    this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
   }
 
   siguienteNivel(){
+    this.subnivel = 0
+    this.nombreAtributo = 'valor'
     this.iluminarSecuencia()
+    this.agregarEventosClick()
   }
 
   transformarNumeroAColor(numero){
@@ -39,6 +45,19 @@ class Juego {
         return 'naranja'
       case 3:
         return 'verde'
+    }
+  }
+
+  transformarColorNumero(numero){
+    switch (numero) {
+      case 'celeste':
+        return 0
+      case 'violeta':
+        return 1
+      case naranja:
+        return 2
+      case 'verde':
+        return 3
     }
   }
 
@@ -57,6 +76,41 @@ class Juego {
   apagarColor(color){
     this.colores[color].classList.remove('light')
   }
+
+  agregarEventosClick() {
+    this.colores.celeste.addEventListener('click', this.elegirColor)
+    this.colores.verde.addEventListener('click', this.elegirColor)
+    this.colores.violeta.addEventListener('click', this.elegirColor)
+    this.colores.naranja.addEventListener('click', this.elegirColor)
+  }
+
+  eliminarEventosClick(){
+    this.colores.celeste.removeEventListener('click', this.elegirColor)
+    this.colores.verde.removeEventListener('click', this.elegirColor)
+    this.colores.violeta.removeEventListener('click', this.elegirColor)
+    this.colores.naranja.removeEventListener('click', this.elegirColor)
+  }
+
+  elegirColor(ev){
+    const nombreColor = ev.target.dataset.color
+    const numeroColor = this.transformarColorNumero(nombreColor)
+    this.ilumunarColor(nombreColor)
+    if (numeroColor === this.secuencia[this.subnivel]) {
+      this.subnivel++
+      if (this.subnivel === this.nivel) {
+        this.nivel++
+        this.eliminarEventosClick()
+        if (this.nivel === (ULTIMO_NIVEL + 1)) {
+          // Gano
+        }else {
+          setTimeout(this.siguienteNivel, 1500)
+        }
+      }
+    }else {
+      // Perdio
+    }
+  }
+
 }
 
 function empezarJuego() {
